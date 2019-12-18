17a11aa0b212607d3c53e59624d9433e84ba8455
Jason Leach <jlspursfan@gmail.com>
2019-12-17 23:08:42 -0800

    tried to convert to Pusher Channels

--- server/server.js
+++ server/server.js
@@ -1,50 +1,50 @@
-require('dotenv').config({ path: '.env' });
+var express = require('express');
+    var bodyParser = require('body-parser');
+    var Pusher = require('pusher');
 
-    const express = require('express');
-    const bodyParser = require('body-parser');
-    const cors = require('cors');
-    const Chatkit = require('@pusher/chatkit-server');
-
-    const app = express();
-
-    const chatkit = new Chatkit.default({
-      instanceLocator: process.env.CHATKIT_INSTANCE_LOCATOR,
-      key: process.env.CHATKIT_SECRET_KEY,
-    });
-
-    app.use(cors());
+    var app = express();
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.urlencoded({ extended: false }));
 
-    app.post('/users', (req, res) => {
-      const { userId } = req.body;
+    // enable cross-origin resource sharing
+   app.use(function(req, res, next) {
+     res.header("Access-Control-Allow-Origin", "*");
+     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+     next();
+   });
 
-      chatkit
-        .createUser({
-          id: userId,
-          name: userId,
-        })
-        .then(() => {
-          res.sendStatus(201);
-        })
-        .catch(err => {
-          if (err.error === 'services/chatkit/user_already_exists') {
-            console.log(`User already exists: ${userId}`);
-            res.sendStatus(200);
-          } else {
-            res.status(err.status).json(err);
-          }
-        });
+
+   var pusher = new Pusher({ // connect to pusher
+      appId: "918580",
+      key: "f47550c0335c0d83863f",
+      secret:  "836c4c3afd79a63f4803",
+      cluster: "us3",
     });
 
-    app.post('/authenticate', (req, res) => {
-      const authData = chatkit.authenticate({
-        userId: req.query.user_id,
-      });
-      res.status(authData.status).send(authData.body);
+    // app_id = "918580"
+    // key = "f47550c0335c0d83863f"
+    // secret = "836c4c3afd79a63f4803"
+    // cluster = "us3"
+
+// Next, add a route for checking if the server is running:
+
+    app.get('/', function(req, res){ // to test if the server is running
+      res.send('all green');
     });
 
-    app.set('port', process.env.PORT || 5200);
-    const server = app.listen(app.get('port'), () => {
-      console.log(`Express running → PORT ${server.address().port}`);
+// Lastly, add the routes for authenticating users. Later on, the Chat component will hit this route every time a visitor views a blog post:
+
+    // to authenticate users
+    app.post('/pusher/auth', function(req, res) {
+      var socketId = req.body.socket_id;
+      var channel = req.body.channel_name;
+      var origin = req.get('origin');
+    // if(origin == 'YOUR BLOG DOMAIN NAME OR IP'){
+    //   // authenticate the request
+    // }
+      var auth = pusher.authenticate(socketId, channel);
+      res.send(auth);
     });
+
+    var port = process.env.PORT || 5000;
+    app.listen(port);
